396b07cfe5d93924e208b35055eac9d442c0a239
peti446 <peti446@gmail.com>
2018-08-04 20:08:48 +0100

    Starter Localization

---
+++ localization/Loc.Core.lua
@@ -0,0 +1,13 @@
+local _, addon = ...
+
+--Create the table for localizations
+addon.L = {}
+
+local function LocalizationNotFound(L, key)
+    -- Return default key
+    return key;
+end
+--Set meta table
+setmetatable(addon.L, {__index=LocalizationNotFound})
+
+-- Set default Localization (English)
