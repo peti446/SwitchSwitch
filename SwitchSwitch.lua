d2b5deb770f30a7617fac77bfccda54814393005
peti446 <peti446@users.noreply.github.com>
2020-12-07 23:39:51 +0000

    Version 1.65

--- SwitchSwitch.lua
+++ SwitchSwitch.lua
@@ -5,7 +5,7 @@ local _, addon = ...
 
 addon.G = {}
 addon.G.SwitchingTalents = false
-addon.version = "1.64"
+addon.version = "1.65"
 addon.CustomProfileName = "custom"
 
 --##########################################################################################################################
