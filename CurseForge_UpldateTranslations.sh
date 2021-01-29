cb1a373a4d519fa5c161cb9babc867bc168ec401
peti446 <peti446@users.noreply.github.com>
2021-01-29 00:50:37 +0000

    CI: added automatic curse forge translation upload script

---
+++ CurseForge_UpldateTranslations.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+
+if [ -f ".env" ]; then
+	. ".env"
+fi
+
+declare -A translationStrings
+translationTempFile=$(mktemp)
+regex='L\[\"(.+)\"\]'
+projectID=299998
+
+process_addon_translations(){
+while read -r line || [ -n "$line" ] ; do
+    if [[ $line =~ $regex ]]
+    then
+        local matchKey="${BASH_REMATCH[1]}"
+        translationStrings["$matchKey"]=true
+    fi
+done < <(cat "$1");
+}
+
+while IFS= read -r -d '' line; do 
+	process_addon_translations "$line"
+done < <(find . -type f -name "*.lua" -not -path "*/Locales/*" -not -path "*/Libs/*" -print0)
+
+for x in "${!translationStrings[@]}"; do
+	printf "L[\"%s\"] = %s\n" "$x" "${translationStrings[$x]}" >> "$translationTempFile"
+done
+
+#language: "enUS", //[enUS, deDE, esES, ect], Required, No Default
+#       namespace: "toc", //Any namespace name, comma delimited. Default: Base Namespace
+#       formatType: TableAdditions, //['GlobalStrings','TableAdditions','SimpleTable']. Default: TableAdditions
+#       missing-phrase-handling: DoNothing //['DoNothing', 'DeleteIfNoTranslations', 'DeleteIfTranslationsOnlyExistForSelectedLanguage', 'DeletePhrase']. Default: DoNothing
+#   localizations: "Localizations To Import"
+
+result=$( curl -sS -0 -X POST -w "%{http_code}" \
+-H "X-Api-Token: $CF_API_KEY" \
+-F "metadata={ language: \"enUS\", formatType: \"TableAdditions\", \"missing-phrase-handling\": \"DeletePhrase\" }" \
+-F "localizations=<$translationTempFile" \
+"https://www.curseforge.com/api/projects/$projectID/localization/import"
+) || exit 1
+
+case $result in
+200)
+  echo "Exported localisation string to curse forge."
+  exit 0
+  ;;
+*)
+  echo -e "Error exporting localisation to curse forge. Response:\n $result \n"
+  exit 1
+  ;;
+esac
+
