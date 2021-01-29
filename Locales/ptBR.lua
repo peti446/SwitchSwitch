d1f333b1e701f114af5cef556f89496c2a24431c
peti446 <peti446@users.noreply.github.com>
2021-01-29 00:45:03 +0000

    refact: Changed localisation to be injected at build of package

---
+++ Locales/ptBR.lua
@@ -0,0 +1,4 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("SwitchSwitch", "ptBR", false, true)
+if L then
+--@localization(locale="ptBR", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="comment",  escape-non-ascii=true, handle-subnamespaces="concat")@
+end
---
+++ Locales/ruRU.lua
@@ -0,0 +1,4 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("SwitchSwitch", "ruRU", false, true)
+if L then
+--@localization(locale="ruRU", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="comment",  escape-non-ascii=true, handle-subnamespaces="concat")@
+end
