df3a4d10987f0a50c592334e990a8ee9a0bfa59d
peti446 <peti446@gmail.com>
2023-11-15 20:35:01 +0000

    chore: Added diagnostic empty line

--- Locales/ptBR.lua
+++ Locales/ptBR.lua
@@ -1,4 +1,5 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("SwitchSwitch", "ptBR", false, true)
+---@diagnostic disable: empty-block
 if L then
 --@localization(locale="ptBR", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="comment",  escape-non-ascii=true, handle-subnamespaces="concat")@
 end
